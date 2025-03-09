15d226ec954c181be15a872f69ede25f0a959d19
WillsBath <madhusudhanan121204@gmail.com>
2025-03-09 14:44:37 +0530

    Update weatherScreen.dart

--- lib/weatherScreen.dart
+++ lib/weatherScreen.dart
@@ -2,21 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: WeatherScreen(),
-    );
-  }
-}
-
 class WeatherScreen extends StatefulWidget {
   @override
   _WeatherScreenState createState() => _WeatherScreenState();
